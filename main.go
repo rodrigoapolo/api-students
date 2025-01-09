f39a4dfec2157705a87ab3de0920c38923744c20
rodrigoapolo <rodrigoapolodev@gmail.com>
2025-01-09 16:03:10 -0300

    Create dynamic student in DB

--- main.go
+++ main.go
@@ -1,31 +1,65 @@
 package main
 
 import (
-"errors"
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
-  "log/slog"
-  "net/http"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rodrigoapolo/api-students/db"
 )
 
 func main() {
-  // Echo instance
-  e := echo.New()
+	// Echo instance
+	e := echo.New()
 
-  // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-  // Routes
-  e.GET("/students", getStudents)
+	// Routes
+	e.GET("/students", getStudents)
+	e.POST("/students", createStudents)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
-  // Start server
-  if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
-    slog.Error("failed to start server", "error", err)
-  }
+	// Start server
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
+	}
 }
 
 // Handler
 func getStudents(c echo.Context) error {
-  return c.String(http.StatusOK, "List of all students")
+	return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudents(c echo.Context) error {
+	student := db.Student{}
+	if err := c.Bind(&student); err != nil{
+		return err
+	}
+	db.AddStudent(student)
+	return c.String(http.StatusCreated, "Create students")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, getStud)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("DELETE %s student", id)
+	return c.String(http.StatusOK, deleteStud)
 }
