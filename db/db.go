e075bd65c69cd0bc40c54018c91d7b4735549beb
rodrigoapolo <rodrigoapolodev@gmail.com>
2025-01-14 12:10:56 -0300

    Improve logs

--- db/db.go
+++ db/db.go
@@ -1,9 +1,7 @@
 package db
 
 import (
-	"fmt"
-	"log"
-
+	"github.com/rs/zerolog/log"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -24,7 +22,7 @@ type Student struct{
 func Init() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("student.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal().Err(err).Msgf("Failed to initialize SQLite: %s", err.Error())
 	}
 
 	db.AutoMigrate(&Student{})
@@ -38,10 +36,11 @@ func NewStudentHanler(db *gorm.DB) *StudentHandler{
 
 func (s *StudentHandler) AddStudent(student Student) error{
 	if result := s.DB.Create(&student); result.Error != nil{
+		log.Error().Msg("Error to create student")
 		return result.Error
 	}
 
-	fmt.Println("Create student")
+	log.Info().Msg("Create student")
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	github.com/labstack/echo/v4 v4.13.3
+	github.com/rs/zerolog v1.33.0
 	gorm.io/driver/sqlite v1.5.7
 	gorm.io/gorm v1.25.12
 )
